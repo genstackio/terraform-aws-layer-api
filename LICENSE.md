45001fa2796c9f099af1f44b1d211e6f0bd027eb
Olivier Hoareau <oha@greenberets.io>
2022-05-30 16:09:29 +0200

    chore: up to compatibility to tf 1.2.1 + aws provider 4.16.0

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2021 GenStack.io (Olivier Hoareau) 
+Copyright (c) 2022 GenStack.io (Olivier Hoareau) 
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "api" {
   source     = "genstackio/apigateway2-api/aws"
-  version    = "0.1.3"
+  version    = "0.2.0"
   name       = var.name
   lambda_arn = module.lambda.arn
 }
 
 module "api-custom-domain" {
   source    = "genstackio/apigateway2-api/aws//modules/domain"
-  version   = "0.1.3"
+  version   = "0.2.0"
   api       = module.api.id
   stage     = module.api.stage
   dns       = var.dns
@@ -20,7 +20,7 @@ module "api-custom-domain" {
 
 module "lambda" {
   source            = "genstackio/lambda/aws"
-  version           = "0.1.11"
+  version           = "0.3.0"
   s3_bucket         = var.package_s3_bucket
   s3_key            = var.package_s3_key
   file              = var.package_file
